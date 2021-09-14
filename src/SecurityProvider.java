81e8a6268ab7d0fcd960db85fdd4540a88d6b9fc
alphakys <gys1120@naver.com>
2021-09-14 11:03:25 +0900

    9.14 동기화

---
+++ src/SecurityProvider.java
@@ -0,0 +1,30 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Cipher;
+
+public class SecurityProvider {
+    
+    public static void main(String[] args)throws NoSuchAlgorithmException{
+        String str = "AlphaMalwewwe";
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(str.getBytes());
+        
+        StringBuilder sb = new StringBuilder();
+
+        byte[] bytes = md.digest();
+
+        for(byte b : bytes){
+            sb.append(String.format("%02x", b));
+        }
+
+        System.out.println(sb);
+
+        Cipher c = Cipher.getInstance("AES");
+        c.init(ENCRYPT_MODE, key);
+
+
+
+
+    }
+}
