accb35d15d58388082d60dbf2972137c3742404c
alphakys <gys1120@naver.com>
2021-09-22 20:02:38 +0900

    고생했다

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,78 @@
+import java.io.BufferedOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+class ClientReader extends Thread{
+    Socket clientSocket;
+
+    ClientReader(Socket clientSocket){
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            InputStream is = clientSocket.getInputStream();
+            byte[] byteArray = new byte[256];
+            
+            while(true){
+                int size = is.read(byteArray);
+                String readMessage = new String(byteArray, 0, size,"UTF-8");
+                System.out.println(">"+ readMessage);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
 public class Client {
     
+    public static void main(String[] args){
+        System.out.println("Client 시작");
+        
+        try {
+            
+            Socket clientSocket = null;
+          
+            Scanner sc = new Scanner(System.in);
+
+            clientSocket = new Socket();
+            clientSocket.connect(new InetSocketAddress(InetAddress.getLocalHost(), 4040));
+            System.out.println("접속 성공");
+            //System.out.println("localPort ==> "+clientSocket.getLocalPort());
+           
+            System.out.println("채팅방에 참여할 이름을 입력하세요 : ");
+            String name = sc.nextLine();
+
+            while(true){
+                ClientReader clientReader = new ClientReader(clientSocket);
+                clientReader.start();
+
+                String sendMessage = sc.nextLine();
+                sendMessage = name+" : "+sendMessage;
+                byte[] byteArray = sendMessage.getBytes("UTF-8");
+
+                OutputStream os = clientSocket.getOutputStream();
+                BufferedOutputStream bos = new BufferedOutputStream(os);
+
+                bos.write(byteArray);
+                bos.flush();
+                System.out.println("<<나>>" +sendMessage);
+                
+            }
+        } catch (Exception e) {
+            e.printStackTrace();        
+        }
+
+        System.out.println("Client 종료");
+        
+    }
     
 }
--- src/Server.java
+++ src/Server.java
@@ -1,24 +1,28 @@
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
   
     class UserInfo{
-        Socket serverSocket;
+        Socket clientSocket;
 
-        UserInfo(Socket serverSocket){
-            this.serverSocket = serverSocket;
+        UserInfo(Socket clientSocket){
+            this.clientSocket = clientSocket;
+            //System.out.println(clientSocket.getPort());
         }
     }
 
     class UserThread extends Thread{
-        Socket serverSocket;
+        Socket clientSocket;
         List<UserInfo> li = new ArrayList<UserInfo>();
 
-        UserThread(Socket serverSocket, List li){
-            this.serverSocket = serverSocket;
+        UserThread(Socket clientSocket, List<UserInfo> li){
+            this.clientSocket = clientSocket;
             this.li = li;
         }
 
@@ -29,7 +33,7 @@ import java.util.List;
                 
                 while(true){
 
-                    InputStream is = serverSocket.getInputStream();
+                    InputStream is = clientSocket.getInputStream();
                     byte[] byteArray = new byte[256];
                     int size = is.read(byteArray);
 
@@ -38,11 +42,12 @@ import java.util.List;
                     }
 
                     String message = new String(byteArray,0,size,"UTF-8");
-                    System.out.println(message);
-
+                    System.out.println("SERVER : "+message);
+                    
+                    //여러 클라이언트들에게 메세지를 보내기 위한 루프
                     for(int i=0; i<li.size(); i++){
-                        if(li.get(i).serverSocket !=serverSocket){
-                            OutputStream os = li.get(i).serverSocket.getOutputStream();
+                        if(li.get(i).clientSocket !=clientSocket){
+                            OutputStream os = li.get(i).clientSocket.getOutputStream();
                             os.write(byteArray);
                         }
                     }
@@ -52,7 +57,7 @@ import java.util.List;
             } catch (Exception e) {
                 System.out.println("Client 접속종료");
                 for(int i=0; i<li.size();){
-                    if(serverSocket == li.get(i).serverSocket){
+                    if(clientSocket == li.get(i).clientSocket){
                         li.remove(i);
                     }else{
                         i++;
@@ -75,11 +80,12 @@ import java.util.List;
         public void run() {
             try {
                 while(true){
-                    Socket serverSocket = mainServerSocket.accept();
+                    Socket clientSocket = mainServerSocket.accept();
                     System.out.println("Client 접속");
-
-                    li.add(new UserInfo(serverSocket));
-                    UserThread userThread = new UserThread(serverSocket, li));
+                    System.out.println("Port ==> "+clientSocket.getPort());
+                    System.out.println("===================================================");
+                    li.add(new UserInfo(clientSocket));
+                    UserThread userThread = new UserThread(clientSocket, li);
                     userThread.start();
                 }
             } catch (Exception e) {
@@ -87,8 +93,6 @@ import java.util.List;
             }
     
         }
-
-        
     }
 
 public class Server {
@@ -98,10 +102,19 @@ public class Server {
         try {
             ServerSocket mainServerSocket = null;
             mainServerSocket = new ServerSocket();
+            mainServerSocket.bind(new InetSocketAddress(InetAddress.getLocalHost(),4040));
+
+            ConnectThread connectThread = new ConnectThread(mainServerSocket);
+            connectThread.start();
+
+            Scanner sc = new Scanner(System.in);
+            int temp = sc.nextInt();
+
         } catch (Exception e) {
-            
+            e.printStackTrace();
         }
 
+        System.out.println("Server end");
 
 
     }
